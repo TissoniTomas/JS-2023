9c3fefa5e2e707969a3a7ecb77bd4d10d918e2ee
TissoniTomas <tissonitomas5@gmail.com>
2023-07-22 00:46:46 -0300

    Agregamos maps

--- codigo.js
+++ codigo.js
@@ -51,9 +51,9 @@ const pedidosRestaurante = [];
 if (seccionEmpleado == "A") {
   tomaPedido(0, sectorA);
 } else if (seccionEmpleado == "B") {
-  tomaPedido(13, sectorB);
+  tomaPedido(12, sectorB);
 } else if (seccionEmpleado == "C") {
-  tomaPedido(25, sectorC);
+  tomaPedido(24, sectorC);
 } else {
   alert(
     "Por favor, ingrese una opcion valida. Recuerde que las opciones validas son A-B-C"
@@ -71,54 +71,39 @@ function tomaPedido(numeroMesa, sector) {
     for (i = 1; i <= 12; i++) {
       
       
-      alert("A continuacion se solicitara el ingreso del pedido de la mesa" + (i + numeroMesa));
-      ordenMesa =  prompt("Si quiere seleccionar la mesa siguiente, deje la casilla en blanco. Si quiere volver al inicio, digite FINALIZAR")
-                  if (ordenMesa.toUpperCase() === finTurno){
-                       break;
-                   }
+      alert("A continuacion se solicitara el ingreso del pedido de la mesa " + (i + numeroMesa));
+      let numeroMesaPedido = (numeroMesa + i)
       let entradaPedido = prompt("Ingrese la entrada solicitada");
       let platoPrincipalPedido = prompt(
         "Ingrese el plato principal solicitado"
-      );
-      let postrePedido = prompt("Ingrese el postre solicitada");
-      let bebidaPedido = prompt("Ingrese la bebida solicitada");
-
-      const pedidoMesa = new Pedido(
-        numeroMesa,
-        entradaPedido,
-        platoPrincipalPedido,
-        postrePedido,
-        bebidaPedido
-      );
-
-      pedidosRestaurante.push(pedidoMesa);
+        );
+        let postrePedido = prompt("Ingrese el postre solicitada");
+        let bebidaPedido = prompt("Ingrese la bebida solicitada");
+        
+        const pedidoMesa = new Pedido(
+          numeroMesaPedido,
+          entradaPedido,
+          platoPrincipalPedido,
+          postrePedido,
+          bebidaPedido
+          );
+          
+          pedidosRestaurante.push(pedidoMesa);
+          ordenMesa =  prompt("Si quiere seleccionar la mesa siguiente, deje la casilla en blanco. Si quiere volver al inicio, digite FINALIZAR")
+                      if (ordenMesa.toUpperCase() === finTurno){
+                           break;
+                       }
     }
-    console.log(ordenMesa);
+    
   } while (ordenMesa !== finTurno);
 }
 
 pedidosRestaurante.map((atributo) => [
   alert(
-    `El pedido consta de :\n Entrada: ${atributo.entrada} \n Plato Principal:  ${atributo.platoPrincipal} \n Bebida: ${atributo.bebida} \n Postre: ${atributo.postre}`
+    `El pedido de la mesa numero ${atributo.numeroMesa} consta de :\n Entrada: ${atributo.entrada} \n Plato Principal:  ${atributo.platoPrincipal} \n Bebida: ${atributo.bebida} \n Postre: ${atributo.postre}`
   ),
 ]);
 
-// function tomaPedido(numeroMesa, sector){
-//     alert("Su sector de trabajo sera "+ sector);
-//     let ordenMesa = ""
 
-//      do {
-//         console.log(ordenMesa);
-//         for(i=1; i<=12; i++){
-//             ordenMesa =  Si quiere seleccionar la mesa siguiente, deje la casilla en blanco. Si quiere volver al inicio, digite FINALIZAR")
-//             if (ordenMesa.toUpperCase() === finTurno){
-//                 break;
-//             }
-//             alert(" La orden de la mesa " +(i + numeroMesa) + " es " +ordenMesa);
-//            }
-//             console.log(ordenMesa);
-//      } while (ordenMesa !== finTurno);
-
-// }
 
 alert("Gracias por usar mi sistema");
